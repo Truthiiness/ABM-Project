644ab1655b098718ffd7898ffe0d522a2dd62e05
Chris Fritz <fritzofgalatia@gmail.com>
2020-03-13 19:32:55 -0400

    13MAR20 (Update 3)
    Changed the model step baseline again for better conditional learning.

--- Model.py
+++ Model.py
@@ -6,7 +6,7 @@ Created on Sat Mar  7 16:01:55 2020
 """
 
 from mesa import Model
-from Agent import Agent1AA, Agent1BB
+from Agent import Agent1AA
 from mesa.time import RandomActivation
 
 class Model(Model):
@@ -18,8 +18,6 @@ class Model(Model):
         for i in range(self.num_agents):
             a = Agent1AA(i, self)
             self.schedule.add(a)
-            b = Agent1BB(i, self)
-            self.schedule.add(b)
 
     def step(self):
         '''Advance the model by one step.'''
