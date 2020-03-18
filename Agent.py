0ef45d35cc05c334ff565922613991fb16260c87
Chris Fritz <fritzofgalatia@gmail.com>
2020-03-18 12:16:23 -0400

    March 18th (Update 1)
    Failed attempt to get multiple types of agents to interact.

--- Agent.py
+++ Agent.py
@@ -6,24 +6,36 @@ Created on Sat Mar  7 16:01:40 2020
 """
 
 from mesa import Agent
+from mesa.space import MultiGrid
+from mesa.space import NetworkGrid
+import networkx as nx
 
-class Agent1AA(Agent):
+class Org(Agent):
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+
+class Agent2(Org):
+    #placeholder agent for testing purposes
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.trait1 = 100
-        self.trait2 = 100
-        self.trait3 = 100
-        self.trait4 = 100
-        self.trait5 = 100
-        self.trait6 = 100
-        self.trait7 = 100
-        self.trait8 = 100
-        self.trait9 = 100
-        self.trait10 = 100
-        self.trait11 = 100
-        self.trait12 = 100
+        self.trait1 = 20
+        self.trait2 = 20
+        self.trait3 = 20
+        self.trait4 = 20
+        self.trait5 = 20
+        self.trait6 = 20
+        self.trait7 = 20
+        self.trait8 = 20
+        self.trait9 = 20
+        self.trait10 = 20
+        self.trait11 = 20
+        self.trait12 = 20
         
     def step(self):
+        self.move()
         if self.trait1 > 0:
             other_agent = self.random.choice(self.model.schedule.agents)
             if other_agent.trait1 == 0:
@@ -132,3 +144,419 @@ class Agent1AA(Agent):
             elif self.trait12 - other_agent.trait12 > 0:
                 other_agent.trait12 = self.trait12
             self.trait12 += 0.5
+
+class Agent1A(Org):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+        
+    def step(self):
+        self.move()
+        if self.trait1 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait1 == 0:
+                return
+            elif self.trait1 - other_agent.trait1 > 4:
+                other_agent.trait1 += 4
+            elif self.trait1 - other_agent.trait1 > 0:
+                other_agent.trait1 = self.trait1
+            self.trait1 += 0.5
+        if self.trait2 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait2 == 0:
+                return
+            elif self.trait2 - other_agent.trait2 > 4:
+                other_agent.trait2 += 4
+            elif self.trait2 - other_agent.trait2 > 0:
+                other_agent.trait2 = self.trait2
+            self.trait2 += 0.5
+        if self.trait3 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait3 == 0:
+                return
+            elif self.trait3 - other_agent.trait3 > 4:
+                other_agent.trait3 += 4
+            elif self.trait3 - other_agent.trait3 > 0:
+                other_agent.trait3 = self.trait3
+            self.trait3 += 0.5
+        if self.trait4 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait4 == 0:
+                return
+            elif self.trait4 - other_agent.trait4 > 4:
+                other_agent.trait4 += 4
+            elif self.trait4 - other_agent.trait4 > 0:
+                other_agent.trait4 = self.trait4
+            self.trait4 += 0.5
+        if self.trait5 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait5 == 0:
+                return
+            elif self.trait5 - other_agent.trait5 > 4:
+                other_agent.trait5 += 4
+            elif self.trait5 - other_agent.trait5 > 0:
+                other_agent.trait5 = self.trait5
+            self.trait5 += 0.5
+        if self.trait6 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait6 == 0:
+                return
+            elif self.trait6 - other_agent.trait6 > 4:
+                other_agent.trait6 += 4
+            elif self.trait6 - other_agent.trait6 > 0:
+                other_agent.trait6 = self.trait6
+            self.trait6 += 0.5
+        if self.trait7 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait7 == 0:
+                return
+            elif self.trait7 - other_agent.trait7 > 4:
+                other_agent.trait7 += 4
+            elif self.trait7 - other_agent.trait7 > 0:
+                other_agent.trait7 = self.trait7
+            self.trait7 += 0.5
+        if self.trait8 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait8 == 0:
+                return
+            elif self.trait8 - other_agent.trait8 > 4:
+                other_agent.trait8 += 4
+            elif self.trait8 - other_agent.trait8 > 0:
+                other_agent.trait8 = self.trait8
+            self.trait8 += 0.5
+        if self.trait9 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait9 == 0:
+                return
+            elif self.trait9 - other_agent.trait9 > 4:
+                other_agent.trait9 += 4
+            elif self.trait9 - other_agent.trait9 > 0:
+                other_agent.trait9 = self.trait9
+            self.trait9 += 0.5
+        if self.trait10 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait10 == 0:
+                return
+            elif self.trait10 - other_agent.trait10 > 4:
+                other_agent.trait10 += 4
+            elif self.trait10 - other_agent.trait10 > 0:
+                other_agent.trait10 = self.trait10
+            self.trait10 += 0.5
+        if self.trait11 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait11 == 0:
+                return            
+            elif self.trait11 - other_agent.trait11 > 4:
+                other_agent.trait11 += 4
+            elif self.trait11 - other_agent.trait11 > 0:
+                other_agent.trait11 = self.trait11
+            self.trait11 += 0.5
+        if self.trait12 > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent.trait12 == 0:
+                return
+            if self.trait12 - other_agent.trait12 > 4:
+                other_agent.trait12 += 4
+            elif self.trait12 - other_agent.trait12 > 0:
+                other_agent.trait12 = self.trait12
+            self.trait12 += 0.5
+  
+# currently inactive Agents while I try to get the model to work #          
+class Agent1B(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1C(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1D(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1E(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 0
+        self.trait2 = 0
+        self.trait3 = 0
+        self.trait4 = 0
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1F(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 0
+        self.trait2 = 0
+        self.trait3 = 0
+        self.trait4 = 0
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1G(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 0
+        self.trait2 = 0
+        self.trait3 = 0
+        self.trait4 = 0
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1H(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1I(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1J(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1K(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1L(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1M(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1N(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1O(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
+
+class Agent1P(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1Q(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1R(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 100
+        self.trait2 = 100
+        self.trait3 = 100
+        self.trait4 = 100
+        self.trait5 = 100
+        self.trait6 = 100
+        self.trait7 = 100
+        self.trait8 = 100
+        self.trait9 = 0
+        self.trait10 = 0
+        self.trait11 = 0
+        self.trait12 = 0
+
+class Agent1S(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.trait1 = 0
+        self.trait2 = 0
+        self.trait3 = 0
+        self.trait4 = 0
+        self.trait5 = 0
+        self.trait6 = 0
+        self.trait7 = 0
+        self.trait8 = 0
+        self.trait9 = 100
+        self.trait10 = 100
+        self.trait11 = 100
+        self.trait12 = 100
--- Model.py
+++ Model.py
@@ -6,19 +6,49 @@ Created on Sat Mar  7 16:01:55 2020
 """
 
 from mesa import Model
-from Agent import Agent1AA
+from Agent import Agent1A, Agent2
 from mesa.time import RandomActivation
+from mesa.space import NetworkGrid
+from mesa.datacollection import DataCollector
+import networkx as nx
 
-class Model(Model):
-    """A model with some number of agents."""
-    def __init__(self, N):
-        self.num_agents = N
-        self.schedule = RandomActivation(self)
-        # Create agents
-        for i in range(self.num_agents):
-            a = Agent1AA(i, self)
-            self.schedule.add(a)
 
-    def step(self):
-        '''Advance the model by one step.'''
-        self.schedule.step()
+#Attempt at getting just 2 different types of agents to interact using the same method as the wolf/sheep model.
+#Failed miserably, so just moved on to learning the networkgrid method, which is what I need anyway.
+#Kept this hashed-out just in case I needed to reference it.
+
+#from mesa.space import MultiGrid
+#class Model(Model):
+#    height = 20
+#    width = 20
+#    
+#    initial_Agent1A = 100
+#    initial_Agent2 = 100
+#    
+#    
+#    def __init__(self, height, width, initial_Agent1A=100, initial_Agent2=100):
+#        self.height = height
+#        self.width = width
+#        self.initial_Agent1A = initial_Agent1A
+#        self.initial_Agent2 = initial_Agent2
+#        self.schedule = RandomActivation(self)
+#        self.grid = (MultiGrid(self.height, self.width, torus=True))
+#    
+#        for i in range(self.initial_Agent1A):
+#            x = self.random.randrange(self.width)
+#            y = self.random.randrange(self.height)
+#            self.grid.place_agent(Agent1A, (x,y))
+#            self.schedule.add(Agent1A)
+#
+#        for i in range(self.initial_Agent2):
+#            x = self.random.randrange(self.width)
+#            y = self.random.randrange(self.height)
+#            self.grid.place_agent(Agent2, (x,y))
+#            self.schedule.add(Agent2)
+#    
+#    def step(self):       
+#        self.schedule.step()
+#
+#    def run_model(self, step_count=10):
+#        for i in range(step_count):
+#            self.step()
